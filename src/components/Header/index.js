803d8622990f03ef84564c693715ccf549e8bd07
Matheus Castro <matheuscastroweb@gmail.com>
2019-10-14 15:31:27 -0300

    First commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.css'
+
+//Stateless components
+const Header = () => (
+    <header id="header-main">React JS simple</header>
+);
+
+ export default Header;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//Utilizar uma única vez em todo o código
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
